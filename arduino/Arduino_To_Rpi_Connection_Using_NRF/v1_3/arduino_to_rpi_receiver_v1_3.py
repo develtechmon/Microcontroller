5dba1329848432721f72e6bcdfa11224896a6a4e
Captluke2328 <125783320+develtechmon@users.noreply.github.com>
2023-05-25 01:03:01 +0800

    +added v1_3

---
+++ arduino/Arduino_To_Rpi_Connection_Using_NRF/v1_3/arduino_to_rpi_receiver_v1_3.py
@@ -0,0 +1,67 @@
+'''
+Work flow
+1. To read accelerometer value from arduino
+2. Use this accelerometer to define the drone
+direction
+3. Add dronekit code
+'''
+
+import time
+from RF24 import RF24, RF24_PA_HIGH
+import struct
+
+pipes = [0xF0F0F0F0E1, 0xF0F0F0F0D2]
+
+radio = RF24(22, 0)  # Set CE and CSN pins
+
+radio.begin()
+radio.openWritingPipe(pipes[1])  # Set the writing pipe
+radio.openReadingPipe(1, pipes[0])  # Set the reading pipe
+radio.setPALevel(RF24_PA_HIGH)  # Set power level to high
+
+radio.setAutoAck(False);
+radio.enableDynamicPayloads()
+radio.setRetries(5,15)
+radio.startListening()
+
+while True:
+    if radio.available():
+        x = radio.read(radio.getDynamicPayloadSize())  # Read the received message
+        x.decode('utf-8')
+        #print(x[0], x[1])
+        print(x[0])
+        
+        if x[0] == "g":
+            print("Enter Guided Mode")
+            
+        elif x[0] == "h":
+            print("Enter Stabilized Mode")
+            
+        elif x[0] == "u":
+            print("Enter Takeoff Mode")
+        
+        elif x[0] == "d":
+            print("Go Right")
+        
+        elif x[0] == "a":
+            print("Go Left")
+        
+        elif x[0] == "s":
+            print("Go Back")
+        
+        elif x[0] == "w":
+            print("Go Forward")
+        
+        elif x[0] == "l":
+            print("Enter Land Mode")
+        
+        elif x[0] == "h":
+            print("Enter Hover Mode")
+            
+        elif x[0] == "q":
+            print("Yaw Left")
+        
+        elif x[0] == "e":
+            print("Yaw Right")
+        
+ 
---
+++ arduino/Arduino_To_Rpi_Connection_Using_NRF/v1_3/arduino_to_rpi_receiver_v1_3/arduino_to_rpi_receiver_v1_3.ino
@@ -0,0 +1,88 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+RF24 radio(7, 8); // Set CE and CSN pins
+const uint64_t pipes[2] = { 0xF0F0F0F0E1LL, 0xF0F0F0F0D2LL };
+
+byte Array[2];
+
+void setup() {
+  Serial.begin(9600);
+  radio.begin();
+  radio.enableDynamicPayloads();
+
+  radio.setRetries(5, 15);
+  radio.openWritingPipe(pipes[0]); // Set the address to send data
+  radio.setPALevel(RF24_PA_HIGH); // Set power level to high
+  radio.stopListening();
+}
+
+void loop() {
+  //Array[0] = 23;
+  //Array[1] = 34;
+
+    if (Serial.available() > 0) {
+    // read incoming serial data:
+    char inChar = Serial.read();
+
+    if (inChar == 'g') {
+      Serial.println("Enter Guided Mode");
+      Array[0] = 'g';
+      
+    }
+
+    else if (inChar == 'h') {
+      Serial.println("Enter Stabilized Mode");
+      Array[0] = 'h';
+    }
+
+    else if (inChar == 'u') {
+      Serial.println("Enter Takeoff Mode");
+      Array[0] = 'u';
+    }
+
+    else if (inChar == 'd') {
+      Serial.println("Go Right");
+      Array[0] = 'd';
+    }
+
+    else if (inChar == 'a') {
+      Serial.println("Go Left");
+      Array[0] = 'a';
+    }
+
+    else if (inChar == 's') {
+      Serial.println("Go Back");
+      Array[0] = 's';
+    }
+
+    else if (inChar == 'w') {
+      Serial.println("Go Forward");
+      Array[0] = 'w';
+    }
+
+    else if (inChar == 'l') {
+      Serial.println("Enter Land Mode");
+      Array[0] = 'l';
+    }
+
+    else if (inChar == 'h') {
+      Serial.println("Enter Hover Mode");
+      Array[0] = 'h';
+    }
+
+    else if (inChar == 'q') {
+      Serial.println("Yaw Left");
+      Array[0] = 'q';
+    }
+
+    else if (inChar == 'e') {
+      Serial.println("Yaw Right");
+      Array[0] = 'e';
+    }
+  
+  radio.write(&Array, sizeof(Array)); // Send the message
+  
+  delay(1);
+}
