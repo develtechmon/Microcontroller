265557efc6b8ee566fa2b4571dbe1bbbae43d732
Captluke2328 <125783320+develtechmon@users.noreply.github.com>
2023-05-25 01:24:49 +0800

    update the script

--- arduino/Arduino_To_Rpi_Connection_Using_NRF/v1_3/arduino_to_rpi_receiver_v1_3/arduino_to_rpi_receiver_v1_3.ino
+++ arduino/Arduino_To_Rpi_Connection_Using_NRF/v1_3/arduino_to_rpi_receiver_v1_3/arduino_to_rpi_receiver_v1_3.ino
@@ -28,53 +28,52 @@ void loop() {
 
     if (inChar == 'g') {
       Serial.println("Enter Guided Mode");
-      Array[0] = 'g';
-      
+      Array[0] = 0;
     }
 
     else if (inChar == 'h') {
       Serial.println("Enter Stabilized Mode");
-      Array[0] = 'h';
+      Array[0] = 1;
     }
 
     else if (inChar == 'u') {
       Serial.println("Enter Takeoff Mode");
-      Array[0] = 'u';
+      Array[0] = 2;
     }
 
     else if (inChar == 'd') {
       Serial.println("Go Right");
-      Array[0] = 'd';
+      Array[0] = 3;
     }
 
     else if (inChar == 'a') {
       Serial.println("Go Left");
-      Array[0] = 'a';
+      Array[0] = 4;
     }
 
     else if (inChar == 's') {
       Serial.println("Go Back");
-      Array[0] = 's';
+      Array[0] = 5;
     }
 
     else if (inChar == 'w') {
       Serial.println("Go Forward");
-      Array[0] = 'w';
+      Array[0] = 6;
     }
 
     else if (inChar == 'l') {
       Serial.println("Enter Land Mode");
-      Array[0] = 'l';
+      Array[0] = 7;
     }
 
     else if (inChar == 'h') {
       Serial.println("Enter Hover Mode");
-      Array[0] = 'h';
+      Array[0] = 8;
     }
 
     else if (inChar == 'q') {
       Serial.println("Yaw Left");
-      Array[0] = 'q';
+      Array[0] = 9;
     }
 
     else if (inChar == 'e') {
@@ -82,6 +81,8 @@ void loop() {
       Array[0] = 'e';
     }
   
+  Serial.println(Array[0]);
+
   radio.write(&Array, sizeof(Array)); // Send the message
   
   delay(1);
